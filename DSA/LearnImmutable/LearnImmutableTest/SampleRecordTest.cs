eb9eeda80aaa2e93e4a0131515a768fabdebe990
BbyWiz <96n_lawrence@outlook.com>
2023-09-05 12:00:46 -0400

    Added unit tests

--- DSA/LearnImmutable/LearnImmutableTest/SampleRecordTest.cs
+++ DSA/LearnImmutable/LearnImmutableTest/SampleRecordTest.cs
@@ -1,5 +1,3 @@
-using LearnImmutable;
-
 namespace LearnImmutableTest
 {
     [TestClass]
@@ -16,7 +14,6 @@ namespace LearnImmutableTest
             Assert.AreEqual(record1, record2);
             Assert.AreNotSame(record1, record2);
         }
-
         [TestMethod]
         public void TestRecordTypeInEqualityWithPositionParameters()
         {
@@ -38,6 +35,50 @@ namespace LearnImmutableTest
             //assert
             Assert.AreEqual(record1, record2);
             Assert.AreSame(record1, record2);
+        }
+
+        [TestMethod]
+        public void TestRecordTypeAutoImplementedProperties()
+        {
+            //Arrange
+            SampleRecord record1 = new SampleRecord(ParamString: "Test", ParamInt: 1, ParamDate: new DateTime(2023, 9, 5));
+
+            //Assert
+            Assert.AreEqual("Test", record1.ParamString);
+            Assert.AreEqual(1, record1.ParamInt);
+            Assert.AreEqual(new DateTime(2023, 9, 5), record1.ParamDate);
+
+        }
+
+        [TestMethod]
+        public void TestRecordTypeCanHaveMutableProperties()
+        {
+            //Arrange
+            string expected = "NewString";
+            SampleRecord record1 =
+                new SampleRecord(ParamString: "Test", ParamInt: 1, ParamDate: new DateTime(2023, 9, 5)) { MutableProperty = "InitialString" };
+            //Act
+            record1.MutableProperty = expected;
+            //Assert
+            Assert.AreEqual(record1.MutableProperty, expected);
+        }
+
+        [TestMethod]
+        public void TestRecordTypeHaveDestructMethodWithOutParam()
+        {
+            //Arrange
+            SampleRecord record1 =
+            new SampleRecord(ParamString: "Test", ParamInt: 1, ParamDate: new DateTime(2023, 9, 5)) { MutableProperty = "InitialString" };
+            string outString = String.Empty;
+            int outInt = 0;
+            DateTime outDateTime = new DateTime();
+
+            //Act
+            record1.Deconstruct(out outString, out outInt, out outDateTime);
+            //Assert
+            Assert.AreEqual(outString, "Test");
+            Assert.AreEqual(outInt, 1);
+            Assert.AreEqual(outDateTime, new DateTime(2023, 9, 5));
         }
     }
 }
--- DSA/LearnImmutable/SampleRecord.cs
+++ DSA/LearnImmutable/SampleRecord.cs
@@ -1,8 +1,8 @@
-﻿using LearnImmutableTest;
-
-
-namespace LearnImmutable
+﻿namespace LearnImmutable
 {
-    public record class SampleRecord(string ParamString, int ParamInt, DateTime ParamDate);
+    public record class SampleRecord(string ParamString, int ParamInt, DateTime ParamDate)
+    {
+        public string? MutableProperty { get; set; }
+    }
 
 }
