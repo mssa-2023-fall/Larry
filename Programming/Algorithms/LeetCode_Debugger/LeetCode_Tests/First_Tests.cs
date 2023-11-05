f6bee0320d132743b1bc164eed03d5c233464e68
BbyWiz <96n_lawrence@outlook.com>
2023-11-04 23:05:42 -0400

    Learning Containers/C#

--- Programming/Algorithms/LeetCode_Debugger/LeetCode_Tests/First_Tests.cs
+++ Programming/Algorithms/LeetCode_Debugger/LeetCode_Tests/First_Tests.cs
@@ -1,11 +1,45 @@
 ﻿namespace LeetCode_Tests
 {
-    public class UnitTest1
+    public class MergeSortedArrayTests
     {
         [Fact]
-        public void Test1()
+        public void Merge_WithNonDecreasingOrder_ReturnsMergedArray()
         {
+            //Arrange
+            int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+            int m = 3;
+            int[] nums2 = { 2, 5, 6 };
+            int n = 3;
+            int[] expected = { 1, 2, 2, 3, 5, 6 };
+
+            //Act
+            MergeSortedArray.Merge(nums1, m, nums2, n);
+
+            //Assert
+            Assert.Equal(expected, nums1);
 
         }
+        [Fact]
+        public void Merge_WithNonDecreasingOrder_DoesNotReturnMergedArray2()
+        {
+            //Arrange
+            int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+            int m = 3;
+            int[] nums2 = { 2, 5, 6 };
+            int n = 3;
+            int[] expected = { 0 };
+            //Act
+            MergeSortedArray.Merge(nums1, m, nums2, n);
+            //Assert
+            Assert.NotEqual(expected, nums1);
+        }
+
     }
 }
+       /* [Fact]
+        public void
+            {
+            }
+        //Arrange
+        //Act
+        //Assert*/
